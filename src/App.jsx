d2a4e07fd4e1f8b5a7bd8d2f85083cb8f2451841
Hirusha Adikari <hirushaadi@gmail.com>
2024-10-31 13:34:25 -0400

    add default config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold underline bg-red-500 inline-block">
       Hello world!
     </h1>
   )
