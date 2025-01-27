4a140af3b56b24a62c8ebe1155258519e705a856
hirusha-adi <hirushaadi@gmail.com>
2025-01-28 00:33:39 +0530

    minify the code

---
+++ src/utils/consts.js
@@ -0,0 +1,9 @@
+export const insDataSections = [
+  { key: "side_effects", title: "Side Effects" },
+  { key: "overview", title: "Overview" },
+  { key: "uses", title: "Uses" },
+  { key: "precautions", title: "Precautions" },
+  { key: "interactions", title: "Interactions" },
+  { key: "origin", title: "Origin" },
+  { key: "daily_intake", title: "Daily Intake" },
+];
