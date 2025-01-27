d816106878adeca8ec0378fb25dc113ab3fe8d43
hirusha-adi <hirushaadi@gmail.com>
2025-01-28 00:50:30 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,29 @@
-const Footer = () => {
+import { Github, BrowserChrome } from "react-bootstrap-icons";
 
+const Footer = () => {
   return (
-    <>
-      Header
-    </>
+    <footer className="py-3">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="flex gap-4">
+          <a
+            href="https://github.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BrowserChrome className="text-2xl" />
+          </a>
+          <a
+            href="https://github.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Github className="text-2xl" />
+          </a>
+        </div>
+        <span className="text-sm">2022 - {new Date().getFullYear()} &copy; Hirusha Adikari</span>
+      </div>
+    </footer>
   );
 };
 
-export { Footer }
+export { Footer };
