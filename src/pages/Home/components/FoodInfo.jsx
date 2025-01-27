3a3a7e9ce46338b23233fc694350119b9ab44c8d
hirusha-adi <hirushaadi@gmail.com>
2025-01-27 23:32:21 +0530

    Update FoodInfo.jsx

--- src/pages/Home/components/FoodInfo.jsx
+++ src/pages/Home/components/FoodInfo.jsx
@@ -3,42 +3,54 @@ import Select from 'react-select';
 import makeAnimated from 'react-select/animated';
 import useLoadJson from "../../../hooks/useLoadJson";
 
-export const colorOptions = [
-  { value: 'ocean', label: 'Ocean', color: '#00B8D9', isFixed: true },
-  { value: 'blue', label: 'Blue', color: '#0052CC', isDisabled: true },
-  { value: 'purple', label: 'Purple', color: '#5243AA' },
-  { value: 'red', label: 'Red', color: '#FF5630', isFixed: true },
-  { value: 'orange', label: 'Orange', color: '#FF8B00' },
-  { value: 'yellow', label: 'Yellow', color: '#FFC400' },
-  { value: 'green', label: 'Green', color: '#36B37E' },
-  { value: 'forest', label: 'Forest', color: '#00875A' },
-  { value: 'slate', label: 'Slate', color: '#253858' },
-  { value: 'silver', label: 'Silver', color: '#666666' },
-];
-
 const FoodInfo = () => {
 
   const [selectedData, setSelectedData] = useState([]);
   const { data: fetchedListData, fetchedListLoading, fetchedListError } = useLoadJson('https://raw.githubusercontent.com/is-it-healthy/data/refs/heads/v2/dist/ins-summary.json');
+  const { data: fetchedAllData, fetchedAllLoading, fetchedAllError } = useLoadJson(selectedData);
 
   const animatedComponents = makeAnimated();
 
   return (
     <>
-      <Select
-        closeMenuOnSelect={false}
-        components={animatedComponents}
-        defaultValue={[]}
-        isMulti
-        // bring from {code, name} to {value, label}
-        // TODO: change the data later to remove this
-        options={fetchedListData.map((item) => ({
-          value: item.code,
-          label: item.name,
-        }))}
-        onChange={setSelectedData}
-        value={selectedData}
-      />
+      <div className="container mx-auto">
+        <div className="text-center">
+          <div className="text-2xl">Food Info</div>
+        </div>
+        <div className="">
+          <Select
+            className="shadow-xl"
+            closeMenuOnSelect={false}
+            components={animatedComponents}
+            defaultValue={[]}
+            isMulti
+            // bring from {code, name} to {value, label}
+            // TODO: change the data later to remove this
+            options={fetchedListData?.map((item) => ({
+              value: item.code,
+              label: item.name,
+              url: `https://raw.githubusercontent.com/is-it-healthy/data/v2/dist/single/${item.code}.json`
+            }))}
+            onChange={setSelectedData}
+            value={selectedData}
+          />
+        </div>
+        <div className="mt-10">
+          <div className="grid grid-cols-2 gap-4">
+            {fetchedAllData?.map((item) => (
+              <div className="card card-compact bg-base-100 shadow-xl w-full">
+                <div className="card-body">
+                  <h2 className="card-title">Shoes!</h2>
+                  <p>If a dog chews shoes whose shoes does he choose?</p>
+                  <div className="card-actions justify-end">
+                    <button className="btn btn-sm btn-primary">Buy Now</button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
