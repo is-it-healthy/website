b1de932645ef718cabe69fb953ea041a8da2593b
Hirusha Adikari <hirushaadi@gmail.com>
2025-10-08 22:51:53 +1100

    final line

--- src/pages/Home/components/Card.jsx
+++ src/pages/Home/components/Card.jsx
@@ -16,6 +16,7 @@ const Card = ({ item }) => {
             <div className="skeleton h-16 w-full"></div>
             <div className="skeleton h-16 w-full"></div>
             <div className="skeleton h-16 w-full"></div>
+            <div className="skeleton h-4 w-3/4"></div>
           </div>
         ) : error ? (
           <>
