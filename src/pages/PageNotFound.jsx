554c51b33530ecc37b9a51f50807d23484f5fbb8
Hirusha Adikari <hirushaadi@gmail.com>
2025-10-09 16:00:26 +1100

    show meaningful message

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,14 +1,25 @@
 import { useEffect } from "react";
+import { ArrowLeft } from "react-bootstrap-icons";
 
 const PageNotFound = () => {
 
   useEffect(() => {
-    document.title = `Page Not Found!`
+    document.title = `Page Not Found! | is it healthy?`
   })
 
   return (
     <>
-      <h1>Page Not Found!</h1>
+      <div className="hero bg-base-100 min-h-screen">
+        <div className="hero-content text-center">
+          <div className="max-w-lg">
+            <h1 className="text-5xl font-bold">404 | Not Found</h1>
+            <p className="py-6">
+              The page that you are looking for does not exist!
+            </p>
+            <button className="btn btn-neutral"><ArrowLeft className="text-lg" />Home</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
